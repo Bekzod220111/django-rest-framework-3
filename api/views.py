51c7f302a09be714e5b0e22dca3b9132fa4d0fa7
Bekzod <tobby4173@gmail.com>
2026-04-09 20:12:01 +0500

    Add Mashina model and register it in admin; update serializers and views; format code and adjust settings

--- api/views.py
+++ api/views.py
@@ -1,26 +1,27 @@
-from rest_framework.views import APIView 
-from rest_framework import status 
-from rest_framework.response import Response 
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 from .models import Telefon
 from .seralizers import TelefonSerializer
+
 # Create your views here.
+
 
 class MyAPi(APIView):
 
-
-    def get(self, request):   
+    def get(self, request):
         telefon = Telefon.objects.all()
         data = TelefonSerializer(telefon, many=True).data
-        return Response(data, status=status.HTTP_200_OK) 
+        return Response(data, status=status.HTTP_200_OK)
 
     def post(self, request):
         ser = TelefonSerializer(data=request.data)
         if ser.is_valid(raise_exception=True):
             ser.save()
             name = ser.name
-        msg = f'{name} telefon qoshildi.'
-        return Response({'message':msg})
-        
+        msg = f"{name} telefon qoshildi."
+        return Response({"message": msg})
+
     def put(self, request):
         try:
             new_name = request.data.get("new_name")
@@ -34,7 +35,4 @@ class MyAPi(APIView):
         except Telefon.DoesNotExist:
             return Response("kechirasiz telefon topilmadi.")
         except Exception as e:
-            return Response("Nomalum xatolik: " + str(e))   
-
-        
-
+            return Response("Nomalum xatolik: " + str(e))
